f5182c3b2ebe90f50bee12f36de2965239f992ef
dkf <dfortunato@gmail.com>
2011-01-29 16:13:13 -0800

    initial commit

---
+++ src/json_extract.c
@@ -0,0 +1,318 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <mysql.h>
+
+#include <yajl/yajl_parse.h>
+
+#if 0
+#define OUT(f, ...) {\
+    if (json_state->current == NULL) { \
+      printf("d: d, ad: d\t"f, ##__VA_ARGS__); \
+    } else { \
+      printf("d: %d, ad: %d\t"f, \
+	     json_state->current->depth, \
+	     json_state->current->arr_depth, ##__VA_ARGS__);	\
+    } \
+  }
+#else 
+#define OUT(f, ...)
+#endif
+
+struct json_el {
+  char *el;
+  int depth;
+  int arr_depth;
+  struct json_el *next;
+  struct json_el *prev;
+};
+
+struct json_state {
+  int done;
+  char *res;
+  int res_len;
+  struct json_el *head;
+  struct json_el *current;
+  struct json_el *last;
+};
+
+static yajl_parser_config parser_config = { 1, 0 };
+
+int handle_null(void *ctx) {
+  struct json_state *json_state = ctx;
+  OUT("NULL\n");
+  if (json_state->done == 1) {
+    strlcpy(json_state->res, "null", 5);
+    json_state->res_len = 4;
+    return 0;
+  } else if (json_state->current->arr_depth > 0) {
+    return 1;
+  } else {
+    json_state->current->depth--;
+  }
+  return 1;
+}
+
+int handle_bool(void *ctx, int b) {
+  struct json_state *json_state = ctx;
+  OUT("BOOL %d\n", b);
+  if (json_state->done == 1) {
+    if (b) {
+      strlcpy(json_state->res, "true", 5);
+      json_state->res_len = 4;
+    } else {
+      strlcpy(json_state->res, "false", 6);
+      json_state->res_len = 5;
+    }
+    return 0;
+  } else if (json_state->current->arr_depth > 0) {
+    return 1;
+  } else {
+    json_state->current->depth--;
+  }
+  return 1;
+}
+
+int handle_num(void *ctx, const char *num, unsigned int len) {
+  struct json_state *json_state = ctx;
+  OUT("NUM\n");
+  if (json_state->done == 1) {
+    if (len > 255) {
+      len = 255;
+    }
+    strlcpy(json_state->res, num, len + 1);
+    json_state->res_len = len;
+    return 0;
+  } else if (json_state->current->arr_depth > 0) {
+    return 1;
+  } else {
+    json_state->current->depth--;
+  }
+  return 1;
+}
+
+int handle_string(void *ctx, const unsigned char *s, unsigned int len) {
+  struct json_state *json_state = ctx;
+  OUT("STRING\n");
+  if (len > 255) {
+    len = 255;
+  }
+  if (json_state->done == 1) {
+    strlcpy(json_state->res, (const char *) s, len + 1);
+    json_state->res_len = len;
+    return 0;
+  }
+  if (json_state->current->arr_depth > 0) {
+    return 1;
+  }
+  json_state->current->depth--;
+  return 1;
+}
+
+int handle_start_map(void *ctx) {
+  struct json_state *json_state = ctx;
+  OUT("START_MAP\n");
+  if (json_state->done == 1) {
+    json_state->done = 0;
+    return 0;
+  }
+  return 1;
+}
+
+int handle_map_key(void *ctx, const unsigned char *s, unsigned int len) {
+  struct json_state *json_state = ctx;
+  OUT("MAP_KEY\n");
+  if (json_state->done == 1) {
+    // shouldn't happen
+    json_state->done = 0;
+    return 0;
+  }
+  if (json_state->current->arr_depth > 0) {
+    return 1;
+  }
+  if (json_state->current->depth == 0 && strncmp((const char *) s, json_state->current->el, len) == 0) {
+    json_state->current = json_state->current->next;
+    if (json_state->current == NULL) {
+      json_state->done = 1;
+    }
+
+  } else {
+    json_state->current->depth++;
+  }
+  return 1;
+}
+
+int handle_end_map(void *ctx) {
+  struct json_state *json_state = ctx;
+  OUT("END_MAP\n");
+  if (json_state->done == 1) {
+    // shouldn't happen
+    json_state->done = 0;
+    return 0;
+  }
+  return 1;
+}
+
+int handle_start_array(void *ctx) {
+  struct json_state *json_state = ctx;
+  OUT("START_ARRAY\n");
+  if (json_state->done == 1) {
+    json_state->done = 0;
+    return 0;
+  }
+  json_state->current->arr_depth++;
+  return 1;
+}
+
+int handle_end_array(void *ctx) {
+  struct json_state *json_state = ctx;
+  OUT("END_ARRAY\n");
+  if (json_state->done == 1) {
+    // shouldn't happen
+    json_state->done = 0;
+    return 0;
+  }
+  json_state->current->arr_depth--;
+  json_state->current->depth--;
+  return 1;
+}
+
+static yajl_callbacks callbacks = {
+  handle_null,
+  handle_bool,
+  NULL,
+  NULL,
+  handle_num,
+  handle_string,
+  handle_start_map,
+  handle_map_key,
+  handle_end_map,
+  handle_start_array,
+  handle_end_array
+};
+
+my_bool json_extract_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+  if (args->arg_count != 2) {
+    strcpy(message, "json_extract() takes two arguments");
+    return 1;
+  }
+  if (args->arg_type[0] != STRING_RESULT) {
+    strcpy(message, "json_extract()'s first argument must be a string");
+    return 1;
+  }
+  if (args->arg_type[1] != STRING_RESULT) {
+    strcpy(message, "json_extract()'s second argument must be a string");
+    return 1;
+  }
+  if (args->args[0] == 0) {
+    strcpy(message, "json_extract()'s first argument must be constant and cannot be null");
+    return 1;
+  }
+  if (args->lengths[0] == 0) {
+    strcpy(message, "json_extract()'s first argument can't be empty");
+    return 1;
+  }
+
+  initid->maybe_null = 1;
+  initid->const_item = 1;
+
+  struct json_state *json_state = calloc(1, sizeof(struct json_state));
+
+  json_state->head = calloc(1, sizeof(struct json_el));
+  json_state->head->depth = 0;
+  json_state->current = json_state->head;
+
+  int last = 0;
+  int i = 0;
+  for (i = 0; i < args->lengths[0]; i++) {
+    if (*(args->args[0] + i) == '.' && last != i) {
+      json_state->current->el = calloc(i - last + 1, sizeof(char));
+      strlcpy(json_state->current->el, args->args[0] + last, i - last + 1);
+      json_state->current->next = calloc(1, sizeof(struct json_el));
+      json_state->current->next->prev = json_state->current;
+      json_state->current = json_state->current->next;
+      json_state->current->depth = 0;
+      last = i + 1;
+    }
+  }
+  if (last != i) {
+    json_state->current->el = calloc(i - last + 1, sizeof(char));
+    strlcpy(json_state->current->el, args->args[0] + last, i - last + 1);
+  } else {
+    // free next
+  }
+
+  struct json_el *cur = json_state->head;
+  while (cur != NULL) {
+    cur = cur->next;
+  }
+
+  json_state->last = json_state->current;
+  initid->ptr = (void *) json_state;
+
+  return 0;
+}
+
+void json_extract_deinit(UDF_INIT *initid) {
+  struct json_state *state = (struct json_state *) initid->ptr;
+  if (state != NULL) {
+    struct json_el *el = state->head;
+    while (el != NULL) {
+      struct json_el *n = el->next;
+      free(el->el);
+      free(el);
+      el = n;
+    }
+    free(initid->ptr);
+  }
+}
+
+char *json_extract(UDF_INIT *initid, UDF_ARGS *args, char *result,
+		       unsigned long *length, char *is_null, char *error) {
+  struct json_state *json_state = (struct json_state *) initid->ptr;
+  json_state->done = 0;
+  json_state->res = result;
+  json_state->current = json_state->head;
+  while (json_state->current != NULL) {
+    json_state->current->depth = 0;
+    json_state->current->arr_depth = 0;
+    json_state->current = json_state->current->next;
+  }
+  json_state->current = json_state->head;
+
+  yajl_handle hand = yajl_alloc(&callbacks, &parser_config, NULL, initid->ptr);
+  yajl_status status;
+
+  status = yajl_parse(hand, (const unsigned char *) args->args[1], args->lengths[1]);
+  switch (status) {
+  case yajl_status_error:
+    *is_null = 1;
+    goto bail;
+  default: /* ok */;
+  }
+
+  if (status != yajl_status_ok && status != yajl_status_client_canceled) {
+    status = yajl_parse_complete(hand);
+    switch (status) {
+    case yajl_status_error:
+    case yajl_status_insufficient_data:
+      *is_null = 1;
+      goto bail;
+    default: /* ok */;
+    }
+  }
+
+  if (json_state->done == 1) {
+    *is_null = 0;
+    *length = json_state->res_len;
+  } else {
+    *is_null = 1;
+  }
+
+ bail:
+  yajl_free(hand);
+  return result;
+}
+
---
+++ test/run_tests.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import sys
+import unittest
+from ctypes import *
+
+l = CDLL(sys.argv[1])
+l.json_extract.restype = c_char_p
+
+STRING_RESULT = c_uint(0)
+
+class UDF_INIT(Structure):
+  _fields_ = [
+    ("maybe_null", c_char),
+    ("decimals", c_uint),
+    ("max_length", c_ulong),
+    ("ptr", c_void_p),
+    ("const_item", c_char),
+    ("extension", c_void_p)]
+
+class UDF_ARGS(Structure):
+  _fields_ = [
+    ("arg_count", c_uint),
+    ("arg_type", POINTER(c_uint)),
+    ("args", POINTER(c_char_p)),
+    ("lengths", POINTER(c_ulong)),
+    ("maybe_null", POINTER(c_char)),
+    ("attributes", POINTER(c_char_p)),
+    ("attribute_lengths", POINTER(c_uint)),
+    ("extension", c_void_p)]
+
+TwoArgsType = c_uint * 2
+TwoArgs = c_char_p * 2
+TwoLengths = c_ulong * 2
+
+c_string = create_string_buffer
+
+class TestBasic(unittest.TestCase):
+  def assertResult(self, expected, spec, s):
+    """expected is None to indicate a null result, False to indicate an error"""
+    initid = UDF_INIT(c_char('\x01'), c_uint(32), c_ulong(100), None, c_char('\x00'), None)
+    args = UDF_ARGS(
+      c_uint(2),
+      TwoArgsType(STRING_RESULT, STRING_RESULT),
+      TwoArgs(c_char_p(spec), c_char_p(s)),
+      TwoLengths(c_ulong(len(spec)), c_ulong(len(s))),
+      pointer(c_char('\x01')), None, None, None)
+
+    self.assertEquals(0, l.json_extract_init(pointer(initid), pointer(args), None))
+
+    length = pointer(c_uint(0))
+    result = c_char_p('\x00' * 255)
+    is_null = pointer(c_char('\x00'))
+    error = pointer(c_char('\x00'))
+
+    actual = l.json_extract(
+          pointer(initid), pointer(args),
+          result, length, is_null, error)
+
+    if error.contents.value == '\x00':
+      if is_null.contents.value == '\x01':
+        self.assertEquals(expected, None)
+      elif is_null.contents.value == '\x00':
+        self.assertEquals(expected, actual)
+      else:
+        self.fail("is_null is %s, not 0 or 1" % is_null.contents.value)
+    elif error.contents.value == '\x01':
+      self.assertEquals(expected, False)
+    else:
+      self.fail("error is %s, not 0 or 1" % error.contents.value)
+
+    l.json_extract_deinit(pointer(initid))
+
+  def test_single_str(self):
+    self.assertResult("foo", "a", "{\"a\":\"foo\"}")
+    self.assertResult("foo", "abcdef", "{\"abcdef\":\"foo\"}")
+  def test_single_null(self):
+    self.assertResult("null", "a", "{\"a\":null}")
+    self.assertResult("null", "abcdef", "{\"abcdef\":null}")
+  def test_single_bool(self):
+    self.assertResult("false", "a", "{\"a\":false}")
+    self.assertResult("true", "abcdef", "{\"abcdef\":true}")
+  def test_single_num(self):
+    self.assertResult("42", "a", "{\"a\":42}")
+    self.assertResult("42.356", "abcdef", "{\"abcdef\":42.356}")
+  def test_bigger(self):
+    self.assertResult("42", "z.bc.def", '{"a":31,"d":[null],"z":{"bc":{"def":42}}}')
+  def test_array_stack_bug(self):
+    self.assertResult("42", "z.bc.def", '{"a":31,"d":[null,2,{"a":1}],"z":{"bc":{"def":42}}}')
+  def test_result_str_too_big(self):
+    self.assertResult("a" * 255, "a", '{"a":"%s"}' % ("a" * 300))
+  def test_result_num_too_big(self):
+    self.assertResult("1" + ("0" * 254), "a", '{"a":"%s"}' % ("1" + ("0" * 300)))
+  def test_array_start(self):
+    self.assertResult(None, "a", '[{"a":1}]')
+  def test_obj_result(self):
+    self.assertResult(None, "a", '{"a":[1]}')
+  def test_map_result(self):
+    self.assertResult(None, "a", '{"a":{"b":1}}')
+
+if __name__ == "__main__":
+  unittest.main(argv=[sys.argv[0]] + sys.argv[2:])
